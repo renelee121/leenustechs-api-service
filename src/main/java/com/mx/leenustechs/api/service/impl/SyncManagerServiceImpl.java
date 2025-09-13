6985a4ae946d95e7bcacb189405a72d7ff46385c
renelee121 <reneleermz_@live.com.mx>
2025-09-13 11:11:57 -0600

    FIX FILTER

--- src/main/java/com/mx/leenustechs/api/service/impl/SyncManagerServiceImpl.java
+++ src/main/java/com/mx/leenustechs/api/service/impl/SyncManagerServiceImpl.java
@@ -25,9 +25,9 @@ public class SyncManagerServiceImpl implements SyncManagerService {
     @Override
     public Mono<GenericEventObjectResponse> consultReactive(String transactionId) {
         Retry retry = Retry.fixedDelay(10, Duration.ofMillis(500))
-                .filter(e -> e instanceof RuntimeException
-                        && e.getMessage() != null
-                        && e.getMessage().contains("Event not found"));
+                .filter(e -> e instanceof RuntimeException 
+                    && e.getMessage() != null 
+                    && e.getMessage().startsWith("Error en GET"));
 
         return Mono.defer(() -> Mono.fromCallable(() -> {
             String json = restClientService.getDataFromService(transactionId);
